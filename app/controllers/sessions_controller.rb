435f6e90e6f2b8baac20e3a00d600f3bb14fa825
Abhi <Abhijith.sa@pitsolutions.com>
2022-05-16 15:58:44 +0530

    controller level restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
+
   def new
+
   end
   def create
     user = User.find_by(username:params[:session][:username])
@@ -18,4 +21,14 @@ class SessionsController < ApplicationController
     flash[:success] = "Logged out successfully"
     redirect_to login_path
   end
+
+  private 
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in"
+      redirect_to root_path
+    end
+  end
+
 end
